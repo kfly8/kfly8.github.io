5d29121dd46075f7c24c18d49b4cb706147d99e0
Kenta, Kobayashi <kentafly88@gmail.com>
2015-12-31 11:36:56 +0900

    rebuilding site 2015年 12月31日 木曜日 11時36分56秒 JST

--- deploy.sh
+++ deploy.sh
@@ -16,5 +16,5 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master
-git subtree push --prefix=public git@github.com:kfly8/kfly8.github.io gh-pages
+git push origin content
+git subtree push --prefix=public git@github.com:kfly8/kfly8.github.io master
